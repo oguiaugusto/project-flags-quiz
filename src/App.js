57a228d7785bd23af501067f39930d9a0541e442
Guilherme <guilhaugusto05@gmail.com>
2021-12-07 16:52:17 -0300

    feat: functions to get countries and randomize it according to continents and amount

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
+import { fetchCountries, getRandomCountries } from './services/fetchCountries';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allCountries: [],
+      countries: [],
+    };
+
+    this.getCountries = this.getCountries.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCountries();
+  }
+
+  async getCountries() {
+    const allCountries = await fetchCountries();
+    this.setState({ allCountries }, () => {
+      const { allCountries } = this.state;
+      this.setState({ countries: getRandomCountries(allCountries, undefined, 10) })
+    });
+  }
+
   render() {
     return (
       <div>App</div>
